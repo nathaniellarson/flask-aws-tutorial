d58b1e4d213dc5be0cb0998fde024cd684fe9a37
Nathaniel Larson <nathaniel.larson@slalom.com>
2020-08-20 18:45:59 -0500

    requirements and URI are updated

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:iVfLwDvKfPP5rv4FL13E@aws-flask-tutorial.cp7tlp79g8xx.us-east-1.rds.amazonaws.com:3306/aws-flask-tutorial'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:iVfLwDvKfPP5rv4FL13E@quiz.c9zzyfawbdwa.us-east-1.rds.amazonaws.com:3306/chicago_quiz'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://apiot:kud0s2023@terraform-20200820224258521800000001.cjsrvztn6fqk.us-east-1.rds.amazonaws.com:3306/apiot'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
